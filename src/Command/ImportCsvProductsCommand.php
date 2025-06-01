3b94d77ed9eb2df5bf3dc340ac642a9882bd1c3a
Jan Bancerewicz <ziomal.kacperox@gmail.com>
2025-06-02 01:13:39 +0200

    Add CSV import to Pimcore and add objects to db

--- src/Command/ImportCsvProductsCommand.php
+++ src/Command/ImportCsvProductsCommand.php
@@ -4,17 +4,14 @@ namespace App\Command;
 
 use Pimcore\Model\DataObject\CategoryItem;
 use Pimcore\Model\DataObject\ProducentInfo;
+use Pimcore\Model\DataObject\ProducentInfo\Listing as ProducentInfoListing;
 use Pimcore\Model\DataObject\Product;
 use Pimcore\Model\Asset;
 use Pimcore\Console\AbstractCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Pimcore\Model\DataObject\Data\ImageGallery;
-use Pimcore\Model\DataObject\Data\ImageGallery\Image;
-use Pimcore\Model\DataObject\AbstractObject;
 use Pimcore\Model\DataObject\CategoryItem\Listing as CategoryItemListing;
-use Pimcore\Model\DataObject\ProducentInfo\Listing as ProducentInfoListing;
-
 
 class ImportCsvProductsCommand extends AbstractCommand
 {
@@ -27,6 +24,9 @@ class ImportCsvProductsCommand extends AbstractCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $skippedProducts = 0;
+        $importedProducts = 0;
+
         $csvPath = PIMCORE_PROJECT_ROOT . '/var/data/import/products.csv';
 
         if (!file_exists($csvPath)) {
@@ -34,80 +34,208 @@ class ImportCsvProductsCommand extends AbstractCommand
             return self::FAILURE;
         }
 
+        // Wczytanie i parsowanie CSV
         $rows = array_map('str_getcsv', file($csvPath));
+        if (empty($rows)) {
+            $output->writeln('<error>Plik CSV jest pusty lub niepoprawny.</error>');
+            return self::FAILURE;
+        }
+
         $header = array_shift($rows);
+        if (!$header) {
+            $output->writeln('<error>Brak nagłówka w pliku CSV.</error>');
+            return self::FAILURE;
+        }
 
         foreach ($rows as $row) {
+            if (count($row) !== count($header)) {
+                $output->writeln('<comment>Niepoprawna liczba kolumn w wierszu, pomijam.</comment>');
+                continue;
+            }
+
             $data = array_combine($header, $row);
 
-            // --- Znajdź lub utwórz kategorię ---
-
-
+            // ################## Znajdź lub utwórz kategorię ######################
             $categoryName = trim($data['Kategoria']);
             $category = null;
 
             if ($categoryName) {
-                // Szukamy kategorii o podanej nazwie i odpowiednim parentId (np. folderze kategorii)
-                $categoryKey = \Pimcore\Model\Element\Service::getValidKey($categoryName, 'object');
-                $existingCategory = CategoryItem::getByPath('/Product Data/Categories/' . $categoryKey); // dopasuj ścieżkę!
+                $list = new CategoryItemListing();
+                $list->setCondition('Kategoria = ?', [$categoryName]);
+                $list->setUnpublished(true);
+                $items = $list->load();
 
-                if ($existingCategory instanceof CategoryItem) {
-                    $category = $existingCategory;
+                //$output->writeln('Szukam kategorii po nazwie: "' . $categoryName . '", znaleziono: ' . count($items));
+
+                if (count($items) > 0) {
+                    $category = $items[0];
+                    //$output->writeln("<comment>Użyto istniejącej kategorii: {$categoryName}</comment>");
                 } else {
-                    $category = new CategoryItem();
-                    $category->setKey($categoryKey);
-                    $category->setParentId(8); // ID folderu kategorii
-                    $category->setKategoria($categoryName);
-                    $category->setOpis('Automatycznie utworzona kategoria');
-                    $category->save();
+                    $output->writeln("<comment>Nie znaleziono kategorii: {$categoryName}, tworzę nową.</comment>");
+                    
+                    //Sprawdź czy obiekt o tym kluczu już istnieje
+                    $categoryKey = \Pimcore\Model\Element\Service::getValidKey($categoryName, 'object');
+                    $categoryParent = \Pimcore\Model\DataObject::getByPath('/Produkcja/Kategorie');
+                    
+                    if (!$categoryParent) {
+                        $output->writeln('<error>Nie znaleziono folderu /Produkcja/Kategorie</error>');
+                        continue;
+                    }
+                    
+                    // Sprawdzenie czy obiekt istnieje po ścieżce
+                    $existingByPath = \Pimcore\Model\DataObject::getByPath('/Produkcja/Kategorie/' . $categoryKey);
+                    
+                    if ($existingByPath instanceof CategoryItem) {
+                        // Jeśli istnieje, ale nie znaleźliśmy go po nazwie, to trzeba zaakutalizować
+                        $output->writeln("<error>Obiekt o kluczu {$categoryKey} istnieje, ale ma inną nazwę. Aktualizuję nazwę.</error>");
+                        $existingByPath->setKategoria($categoryName);
+                        try {
+                            $existingByPath->save();
+                            $category = $existingByPath;
+                            $output->writeln("<info>Zaktualizowano nazwę kategorii na: {$categoryName}</info>");
+                        } catch (\Exception $e) {
+                            $output->writeln("<error>Błąd aktualizacji kategorii: " . $e->getMessage() . "</error>");
+                            continue;
+                        }
+                    } else {
+                        // Tworzenie nowej kategorii, gdy nie ma szukanej
+                        $category = new CategoryItem();
+                        $category->setKey($categoryKey);
+                        $category->setParent($categoryParent);
+                        $category->setKategoria($categoryName);
+                        $category->setOpis('Automatycznie utworzona kategoria');
+                        $category->setPublished(true);
+                        
+                        try {
+                            $category->save();
+                            $output->writeln("<info>Utworzono nową kategorię: {$categoryName}</info>");
+                        } catch (\Exception $e) {
+                            $output->writeln("<error>Błąd podczas zapisywania kategorii: " . $e->getMessage() . "</error>");
+                            
+                            // Jeśli to błąd unikalności, spróbuj znaleźć istniejący obiekt
+                            if (strpos($e->getMessage(), 'unique constraint') !== false) {
+                                $output->writeln("<comment>Próba znalezienia istniejącej kategorii po błędzie unikalności...</comment>");
+                                $retryList = new CategoryItemListing();
+                                $retryList->setCondition('Kategoria = ?', [$categoryName]);
+                                $retryItems = $retryList->load();
+                                
+                                if (count($retryItems) > 0) {
+                                    $category = $retryItems[0];
+                                    $output->writeln("<info>Znaleziono kategorię po ponownym wyszukiwaniu</info>");
+                                }
+                            }
+                            continue;
+                        }
+                    }
                 }
-
             }
 
-            // --- Znajdź lub utwórz producenta ---
+            // ################# Znajdź lub utwórz producenta ###################
             $producer = null;
             $producerName = trim($data['Producent']);
             $producerNip = isset($data['NIP']) ? trim($data['NIP']) : '';
+
             if ($producerName) {
-                $list = new ProducentInfo\Listing();
+                // Szukamy producenta po nazwie (pole Nazwa)
+                $list = new ProducentInfoListing();
                 $list->setCondition('Nazwa = ?', [$producerName]);
+                $list->setUnpublished(true);
                 $items = $list->load();
+
                 if (count($items) > 0) {
                     $producer = $items[0];
+                    //$output->writeln("<comment>Użyto istniejącego producenta: {$producerName}</comment>");
+                    
+                    // Aktualizuj NIP jeśli jest różny
                     if ($producerNip !== '' && $producerNip !== $producer->getNIP()) {
                         $producer->setNIP($producerNip);
-                        $producer->save();
+                        try {
+                            $producer->save();
+                            $output->writeln("<info>Zaktualizowano NIP producenta: {$producerName}</info>");
+                        } catch (\Exception $e) {
+                            $output->writeln("<error>Błąd aktualizacji NIP: " . $e->getMessage() . "</error>");
+                        }
                     }
                 } else {
+                    $output->writeln("<comment>Nie znaleziono producenta: {$producerName}, tworzę nowego.</comment>");
+                    // Tworzenie nowego producenta
                     $producer = new ProducentInfo();
-                    $producer->setKey(\Pimcore\Model\Element\Service::getValidKey($producerName, 'object'));
-                    $producer->setParentId(9); // ID folderu dla producentów
-                    $producer->setNazwa($producerName);
-                    if ($producerNip !== '') {
-                        $producer->setNIP($producerNip);
+                    $producerKey = \Pimcore\Model\Element\Service::getValidKey($producerName, 'object');
+                    $producerParent = \Pimcore\Model\DataObject::getByPath('/Produkcja/Producenci');
+                    
+                    if (!$producerParent) {
+                        $output->writeln('<error>Nie znaleziono folderu /Produkcja/Producenci</error>');
+                        continue;
                     }
-                    $producer->save();
+                    
+                    // Sprawdzenie czy obiekt istnieje po ścieżce
+                    $existingByPath = \Pimcore\Model\DataObject::getByPath('/Produkcja/Producenci/' . $producerKey);
+                    
+                    if ($existingByPath instanceof ProducentInfo) {
+                        $output->writeln("<error>Obiekt o kluczu {$producerKey} istnieje, ale ma inną nazwę. Aktualizuję nazwę.</error>");
+                        $existingByPath->setNazwa($producerName);
+                        if ($producerNip !== '') {
+                            $existingByPath->setNIP($producerNip);
+                        }
+                        try {
+                            $existingByPath->save();
+                            $producer = $existingByPath;
+                            $output->writeln("<info>Zaktualizowano producenta: {$producerName}</info>");
+                        } catch (\Exception $e) {
+                                $output->writeln("<error>Błąd aktualizacji producenta: " . $e->getMessage() . "</error>");
+                                continue;
+                            }
+                    } else {
+                        $producer->setKey($producerKey);
+                        $producer->setParent($producerParent);
+                        $producer->setNazwa($producerName);
+                        if ($producerNip !== '') {
+                            $producer->setNIP($producerNip);
+                        }
+                        $producer->setPublished(true);
+                        
+                        try {
+                            $producer->save();
+                            $output->writeln("<info>Utworzono nowego producenta: {$producerName}</info>");
+                        } catch (\Exception $e) {
+                            $output->writeln("<error>Błąd podczas zapisywania producenta: " . $e->getMessage() . "</error>");
+                            continue;
+                        }
+                        }
+                    }
                 }
-            }
 
-            // --- Utwórz obiekt produktu ---
-            $productList = new \Pimcore\Model\DataObject\Product\Listing();
-            $productList->setCondition('sku = ?', [$data['SKU']]);
-            $existing = $productList->load()[0] ?? null;            
-            if ($existing instanceof Product) {
-                $output->writeln("<comment>Produkt o SKU {$data['SKU']} już istnieje, pomijam.</comment>");
+            // ####################### Utwórz lub sprawdź produkt #######################
+            $sku = trim($data['SKU'] ?? '');
+            if (!$sku) {
+                $output->writeln('<comment>Brak SKU, pomijam wiersz.</comment>');
                 continue;
             }
+
+            $productList = new \Pimcore\Model\DataObject\Product\Listing();
+            $productList->setCondition('sku = ?', [$data['SKU']]);
+            $existing = $productList->load()[0] ?? null;
+
+            if ($existing instanceof Product) {
+                $skippedProducts++;
+                continue; // Pomijamy istniejący produkt
+            }
+
             $product = new Product();
-            $product->setKey(\Pimcore\Model\Element\Service::getValidKey($data['SKU'], 'object'));
-            $product->setParentId(6); // ID folderu na produkty
+            $product->setKey(\Pimcore\Model\Element\Service::getValidKey($sku, 'object'));
+            $productParent = \Pimcore\Model\DataObject::getByPath('/Produkcja/Produkty');
+            if (!$productParent) {
+                $output->writeln('<error>Nie znaleziono folderu /Produkcja/Produkty</error>');
+                continue;
+            }
+            $product->setParent($productParent);
             $product->setPublished(true);
 
-            $product->setSku($data['SKU']);
-            $product->setNazwa($data['Nazwa']);
-            $product->setOpis($data['Opis']);
-            $product->setCena((float)$data['Cena']);
-            $product->setStatus($data['Status']);
+            $product->setSku($sku);
+            $product->setNazwa(trim($data['Nazwa'] ?? ''));
+            $product->setOpis(trim($data['Opis'] ?? ''));
+            $product->setCena(isset($data['Cena']) ? (float)$data['Cena'] : 0.0);
+            $product->setStatus(trim($data['Status'] ?? ''));
 
             if ($category) {
                 $product->setKategoria($category);
@@ -116,44 +244,44 @@ class ImportCsvProductsCommand extends AbstractCommand
                 $product->setProducent($producer);
             }
 
-            // --- Zdjęcia - imageGallery ---
-            $gallery = new ImageGallery();
+            // ####################### Zdjęcia - imageGallery ###########################
+            $galleryItems = [];
             $imageFiles = array_map('trim', explode(',', $data['Zdjecia']));
             foreach ($imageFiles as $imageFile) {
                 $imageFile = trim($imageFile, "\"' ");
                 if ($imageFile) {
                     $asset = Asset::getByPath('/import/' . $imageFile);
                     if ($asset instanceof Asset\Image) {
-                        $img = new Image();
+                        $img = new \Pimcore\Model\DataObject\Data\Hotspotimage();
                         $img->setImage($asset);
-                        $img->setTitle($asset->getFilename());
-                        $gallery->addImage($img);
+                        // Ustawienie podstawowych właściwości
+                        $img->setCrop(null);
+                        $img->setMarker(null);
+                        $img->setHotspots(null);
+                        $galleryItems[] = $img;
                     }
                 }
             }
-            $product->setZdjecia($gallery);
 
-            // --- Dokumentacja - advancedManyToManyRelation ---
-            $docs = [];
-            $docFiles = array_map('trim', explode(',', $data['Dokumentacja']));
-            foreach ($docFiles as $docFile) {
-                $docFile = trim($docFile, "\"' ");
-                if ($docFile) {
-                    $asset = Asset::getByPath('/import/' . $docFile);
-                    if ($asset instanceof Asset\Document) {
-                        $docs[] = $asset;
-                    }
-                }
-            }
-            if (!empty($docs)) {
-                $product->setDokumentacja($docs);
+            if (!empty($galleryItems)) {
+                $product->setZdjecia(new ImageGallery($galleryItems));
             }
 
-            $product->save();
-            $output->writeln("<info>Zaimportowano produkt: {$data['SKU']}</info>");
+            try {
+                $product->save();
+                $importedProducts++;
+                $output->writeln("<info>Zaimportowano nowy produkt: {$data['SKU']}</info>");
+            } catch (\Exception $e) {
+                $output->writeln("<error>Błąd podczas zapisywania produktu {$data['SKU']}: " . $e->getMessage() . "</error>");
+                continue;
+            }
         }
 
-        $output->writeln('<info>Import zakończony pomyślnie!</info>');
+        if ($skippedProducts > 0) {
+            $output->writeln("<comment>Pominięto {$skippedProducts} istniejących produktów</comment>");
+        }
+        $output->writeln("<info>Zaimportowano {$importedProducts} nowych produktów</info>");
+
         return self::SUCCESS;
     }
 }
